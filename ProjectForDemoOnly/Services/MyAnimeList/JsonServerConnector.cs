3d829834211193a32bc7a679964344d50984a242
RudeusMSK <rudeusmsk@gmail.com>
2025-04-11 10:23:22 +0700

    fix:  implement interface member MAL_ConnectorServerType

--- ProjectForDemoOnly/Services/MyAnimeList/JsonServerConnector.cs
+++ ProjectForDemoOnly/Services/MyAnimeList/JsonServerConnector.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ProjectForDemoOnly.Models.Services.MyAnimeListModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace ProjectForDemoOnly.Services.MyAnimeList
@@ -13,6 +15,36 @@ namespace ProjectForDemoOnly.Services.MyAnimeList
         }
 
         public string Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MAL_AnimeInfo> GetAnimeInfoAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MAL_AnimeReview>> GetAnimeReviewAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MAL_Genres>> GetGenresAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MAL_AnimeOfSeason> GetSeasonalAnimeAsync(string season, int? year)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MAL_TopAnime>> GetTopAnimeAsync(string Category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MAL_Recommendations>> Get_RecommendationsAsync(int? page)
         {
             throw new NotImplementedException();
         }
