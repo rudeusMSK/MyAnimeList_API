4d1c21d5e62ac83bc2104851e8df30454c599bb1
RudeusMSK <rudeusmsk@gmail.com>
2025-04-12 22:33:43 +0700

    style: commit code and add SendRequestAsync, CreateHttpRequest fun.

--- ProjectForDemoOnly/Services/MyAnimeList/RapidapiConnector.cs
+++ ProjectForDemoOnly/Services/MyAnimeList/RapidapiConnector.cs
@@ -1,7 +1,9 @@
-﻿using ProjectForDemoOnly.Models.Services.MyAnimeListModel;
+﻿using Newtonsoft.Json;
+using ProjectForDemoOnly.Models.Services.MyAnimeListModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -9,6 +11,25 @@ namespace ProjectForDemoOnly.Services.MyAnimeList
 {
     public class RapidapiConnector : MAL_ConnectorServerType
     {
+        // Config: authentic RapApi.MyAnimeListAPI
+        private readonly HttpClient httpClient;
+        private readonly string nameServer;
+        private readonly string apiKey;
+        private readonly string apivalue;
+
+        public RapidapiConnector(HttpClient httpClient, string nameServer, string apiKey, string apivalue)
+        {
+            this.httpClient = httpClient;
+            this.nameServer = nameServer; // https://myanimelist-api1.p.rapidapi.com/anime/
+            this.apiKey = apiKey;// "X-RapidAPI-Key"
+            this.apivalue = apivalue;// "dcba14be99msh7fda78dd24a8705p1f40b4jsn2874bae46dc6"
+
+            // Config endpint here:
+            // stringbuilder endpoint: name Server + base endpoint + params
+        }
+
+
+        // SERVER
         public string Connect()
         {
             throw new NotImplementedException();
@@ -19,6 +40,8 @@ namespace ProjectForDemoOnly.Services.MyAnimeList
             throw new NotImplementedException();
         }
 
+
+        // API
         public Task<MAL_AnimeInfo> GetAnimeInfoAsync(int? id)
         {
             throw new NotImplementedException();
@@ -39,14 +62,50 @@ namespace ProjectForDemoOnly.Services.MyAnimeList
             throw new NotImplementedException();
         }
 
-        public Task<List<MAL_TopAnime>> GetTopAnimeAsync(string Category)
+        public async Task<List<MAL_TopAnime>> GetTopAnimeAsync(string Category) // Refactor param:  Category, page.
         {
-            throw new NotImplementedException();
+            // Config:
+            const string endpointFormat = "{0}top/{1}?p={2}";
+            
+            //string category = CategoryOptions.all.ToString();
+            int page = 1;
+
+            // Send request:
+            string endpoint = string.Format(endpointFormat, nameServer, Category, page);
+
+            return await SendRequestAsync<List<MAL_TopAnime>>(endpoint);
         }
 
         public Task<List<MAL_Recommendations>> Get_RecommendationsAsync(int? page)
         {
             throw new NotImplementedException();
+        }
+
+        // Process Send Request:
+        private async Task<T> SendRequestAsync<T>(string endpoint)
+        {
+            // Send Request:
+            var request = CreateHttpRequest(endpoint);
+            var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            // Reponses:Json body
+            var body = await response.Content.ReadAsStringAsync();
+
+            // Prosecss body:
+            if (string.IsNullOrWhiteSpace(body))
+                return default; // process ...
+
+            return JsonConvert.DeserializeObject<T>(body);
+        }
+
+        // Create Request
+        private HttpRequestMessage CreateHttpRequest(string endpoint)
+        {
+            return new HttpRequestMessage(HttpMethod.Get, new Uri(endpoint))
+            {
+                Headers = { { apiKey, apivalue } }
+            };
         }
     }
 }
