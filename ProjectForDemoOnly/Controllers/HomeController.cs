dc9fd70cd0f2a7c10cad79297b33de6721fb0bd4
RudeusMSK <rudeusmsk@gmail.com>
2025-05-15 21:30:37 +0700

    feat: <Home Controller View> -  Add view: AnimeDetail, Recommendations, Seasonal, Top - Update: layout navbar redirect Add css: Content - Add Top.css - Update: style.css

--- ProjectForDemoOnly/Controllers/HomeController.cs
+++ ProjectForDemoOnly/Controllers/HomeController.cs
@@ -5,29 +5,68 @@ using System.Web.Mvc;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System;
+using ProjectForDemoOnly.Models.Services.MyAnimeListModel;
+using ProjectForDemoOnly.Services.MyAnimeList;
+using System.Web.Services.Description;
 
 namespace ProjectForDemoOnly.Controllers
 {
     public class HomeController : Controller
     {
+
+        private ErrorViewModel errorView = new ErrorViewModel();
+        IMALServices services;
+
         public ActionResult index()
         {
             return View();
         }
 
-        public ActionResult Top()
+        public async Task<ActionResult> Top()
         {
-            return View();
+            // Init Service:
+            services = AnimeService.InitService(Request);
+
+            if (services == null)
+            {
+                errorView.title = "Chua ket noi service";
+                errorView.Message = "chon kieu ket noi";
+                return PartialView("Error", errorView);
+            }
+            // Get Service:
+                var topAni = await services.GetTopAnimeAsync("all", 1);
+                return View(topAni);
         }
 
-        public ActionResult Recommendations()
+        public async Task<ActionResult> Recommendations()
         {
-            return View();
+            // Init Service:
+            services = AnimeService.InitService(Request);
+
+            if (services == null)
+            {
+                errorView.title = "Chua ket noi service";
+                errorView.Message = "chon kieu ket noi";
+                return PartialView("Error", errorView);
+            }
+            // Get Service:
+            var body = await services.Get_RecommendationsAsync(1);
+            return View(body);
         }
 
-        public ActionResult Seasonal()
+        public async Task<ActionResult> Seasonal()
         {
-            return View();
+            // Init Service:
+            services = AnimeService.InitService(Request);
+            if (services == null)
+            {
+                errorView.title = "Chua ket noi service";
+                errorView.Message = "chon kieu ket noi";
+                return PartialView("Error", errorView);
+            }
+                // Get Service:
+                var AniOfSeasonal = await services.GetSeasonalAnimeAsync(null, 2025);
+                return View(AniOfSeasonal);
         }
 
         public ActionResult Review()
@@ -35,14 +74,54 @@ namespace ProjectForDemoOnly.Controllers
             return View();
         }
 
-        public ActionResult AnimeDetail()
+        public async Task<ActionResult> AnimeDetail()
         {
-            return View();
+            IMALServices services = AnimeService.InitService(Request);
+            if (services == null)
+            {
+                errorView.title = "Chua ket noi service";
+                errorView.Message = "chon kieu ket noi";
+                return PartialView("Error", errorView);
+            }
+            // check id...
+                MAL_AnimeInfo body = await services.GetAnimeInfoAsync(3352);
+                return View(body);
         }
 
         public ActionResult Genres()
         {
             return View();
+        }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+            var ex = filterContext.Exception;
+
+            if (ex is JsonException je)
+            {
+                errorView.title = "Json parsing error";
+                errorView.Message = je.Message;
+            }
+            else
+            {
+                errorView.title = "Exception:";
+                errorView.Message = ex.Message;
+            }
+
+            // Trả về PartialView nếu là AJAX hoặc lấy từ view con
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                filterContext.Result = new PartialViewResult
+                {
+                    ViewName = "Error",
+                    ViewData = new ViewDataDictionary(errorView)
+                };
+            }
+            else
+            {
+                filterContext.Result = View("Error", errorView);
+            }
         }
     }
 }
